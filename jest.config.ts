f52724a0e2262a896708b5f101c6af97cca1e0dd
devgustavovasquez <gustavovasquez2002@gmail.com>
2023-06-13 10:11:32 -0300

    ci: add husky

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,20 @@
 import { Config } from "jest";
-import { pathsToModuleNameMapper } from "ts-jest";
-import { compilerOptions } from "./tsconfig.json";
 
 const config: Config = {
   rootDir: ".",
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: "<rootDir>/",
-  }),
   collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
   coveragePathIgnorePatterns: ["<rootDir>/dist/"],
   coverageDirectory: "coverage",
   transform: {
     ".+\\.ts$": "ts-jest",
+  },
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
   },
 };
 
